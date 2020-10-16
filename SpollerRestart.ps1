b297311a528d652ef7b364597e416bfcc265f262
vkorsh <vkorsh@users.noreply.github.com>
2020-10-16 14:53:25 +0300

    Update SpollerRestart.ps1

--- SpollerRestart.ps1
+++ SpollerRestart.ps1
@@ -1,7 +1,12 @@
-﻿[console]::BackgroundColor = "Black"
-cls
+# Очищаем консоль перед использованием
+Clear-Host
+# легкая раскраска консоли
+[console]::BackgroundColor = "Black"
+# Выводим сообщение пользователю 
 Write-Host `n "Перезапуск спулера сервера" `n -Backgroundcolor DarkGreen
+# Запрашиваем Имя сервера для перезапуска службы
 $server = Read-Host "Введите имя Сервера для перезапуска спулера"
+# Выводим сообщение пользователю 
 Write-Host `n "Проверяем состояние сервиса печати" `n
 Get-Service spooler, cpsvc -computername $server | format-table -property Name, Status, DisplayName
 Write-Host `n "Останавливаем сервисы" `n
